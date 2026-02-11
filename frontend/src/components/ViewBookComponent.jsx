77eb0f9fa25152b8c05adcabdf0fd54ab362d034
jupitters <sampaiobale@gmail.com>
2026-02-11 13:08:38 -0300

    view book component

--- frontend/src/components/ViewBookComponent.jsx
+++ frontend/src/components/ViewBookComponent.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import BookService from "../services/BookService";
 
 class ViewBookComponent extends Component{
     constructor(props) {
         super(props);
 
         this.state = {
-
+            id: this.props.match.params.id,
+            book: {}
         }
+    }
+
+    componentDidMount() {
+        BookService.getBookById(this.state.id).then(res => {
+            this.setState({ book: res.data });
+        })
     }
 
     render() {
         return (
             <div>
-
+                <br><br>
+                <div className="card col-md-6 offset-md-3">
+                    <h3 className="text-center">View Book Details</h3>
+                    <div className="card-body">
+                        <div className="row">
+                            <label>Title: </label>
+                            <div> {this.state.book.name}</div>
+                            <label>Author: </label>
+                            <div> {this.state.book.author}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
