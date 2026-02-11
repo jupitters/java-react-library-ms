6031ca51c8ffc88ea6005114f512b58582d9c643
jupitters <sampaiobale@gmail.com>
2026-02-11 10:45:14 -0300

    header and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ListBookComponent from "./components/ListBookComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="container">
-      <ListBookComponent />
-    </div>
+      <div>
+          <HeaderComponent />
+          <div className="container">
+              <ListBookComponent />
+          </div>
+          <FooterComponent />
+      </div>
   );
 }
 
