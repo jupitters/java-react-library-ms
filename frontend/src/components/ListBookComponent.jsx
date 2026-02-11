b1164c760d713864db0bff6adf8302ca2d7059dd
jupitters <sampaiobale@gmail.com>
2026-02-11 10:10:09 -0300

    book component

--- frontend/src/components/ListBookComponent.jsx
+++ frontend/src/components/ListBookComponent.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import BookService from "../services/BookService";
 
 class ListBookComponent extends Component{
     constructor(props){
@@ -10,7 +11,9 @@ class ListBookComponent extends Component{
     }
 
     componentDidMount() {
-
+        BookService.getBooks().then((res) => {
+            this.setState({ books: res.data });
+        })
     }
 
     render(){
