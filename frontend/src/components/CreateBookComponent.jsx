5e154010e0779dc59e23ea7c87d8d1601ceeadf1
jupitters <sampaiobale@gmail.com>
2026-02-11 11:17:33 -0300

    add book component

--- frontend/src/components/CreateBookComponent.jsx
+++ frontend/src/components/CreateBookComponent.jsx
@@ -5,14 +5,29 @@ class CreateBookComponent extends Component{
         super(props)
 
         this.state = {
-
+            name: '',
+            author: ''
         }
     }
 
     render() {
         return (
             <div>
-                <h1>Book Form</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                            <h3 className="text-center">Add Book</h3>
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label> Name: </label>
+                                        <input placeholder="Name" name="name" className="form-control" value={this.state.name} onChange={this.changeNameHandler} />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
