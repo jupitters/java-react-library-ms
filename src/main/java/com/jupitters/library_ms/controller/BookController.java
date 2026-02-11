268834bef475f8be12b04616b3a4621d3b6c752c
jupitters <sampaiobale@gmail.com>
2026-02-11 10:33:33 -0300

    allow cors

--- src/main/java/com/jupitters/library_ms/controller/BookController.java
+++ src/main/java/com/jupitters/library_ms/controller/BookController.java
@@ -3,12 +3,14 @@ package com.jupitters.library_ms.controller;
 import com.jupitters.library_ms.model.Book;
 import com.jupitters.library_ms.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
