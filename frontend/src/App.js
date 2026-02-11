758c79f56319bb459460ee2bf93b2411b3f589b4
jupitters <sampaiobale@gmail.com>
2026-02-11 10:55:21 -0300

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ListBookComponent from "./components/ListBookComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
@@ -7,11 +8,18 @@ import FooterComponent from "./components/FooterComponent";
 function App() {
   return (
       <div>
-          <HeaderComponent />
-          <div className="container">
-              <ListBookComponent />
-          </div>
-          <FooterComponent />
+          <BrowserRouter>
+              <div className="container">
+                  <HeaderComponent />
+                  <div className="container">
+                      <Switch>
+                          <Route path="/" component={<ListBookComponent />} />
+                          <Route path="/books" component={<ListBookComponent />} />
+                      </Switch>
+                  </div>
+                  <FooterComponent />
+              </div>
+          </BrowserRouter>
       </div>
   );
 }
