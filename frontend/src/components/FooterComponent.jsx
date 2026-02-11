484289a6a5f0401e100b8ce349d89cc4f6d925f7
jupitters <sampaiobale@gmail.com>
2026-02-11 10:42:10 -0300

    footer component

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -12,7 +12,9 @@ class FooterComponent extends Component{
     render(){
         return (
             <div>
-
+                <footer className="footer">
+                    <span className="text-muted">All Rights Reserved </span>
+                </footer>
             </div>
         )
     }
