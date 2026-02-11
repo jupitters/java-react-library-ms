cd35ebd4c5c711a4ec6f6ab457b3baa85ce40d6b
jupitters <sampaiobale@gmail.com>
2026-02-11 09:52:25 -0300

    list book component

--- frontend/src/components/ListBookComponent.jsx
+++ frontend/src/components/ListBookComponent.jsx
@@ -5,14 +5,24 @@ class ListBookComponent extends Component{
         super(props)
 
         this.state = {
-
+            books: []
         }
     }
 
     render(){
         return(
             <div>
-
+                <h2 className="text-center">Books List</h2>
+                <div className="row">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Book Name</th>
+                                <th>Book Author</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         )
     }
