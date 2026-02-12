d9784f8ae8c69599d51dc55084fdeabf97cd9553
jupitters <sampaiobale@gmail.com>
2026-02-12 18:58:26 -0300

    update book updated

--- frontend/src/components/UpdateBookComponent.jsx
+++ frontend/src/components/UpdateBookComponent.jsx
@@ -1,76 +1,59 @@
-import React, { Component } from 'react'
+import React, {  useEffect, useState } from 'react'
 import BookService from "../services/BookService";
 
-class UpdateBookComponent extends Component {
-    constructor(props){
-        super(props)
+const UpdateBookComponent = () => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const [name, setName] = useState('')
+    const [author, setAuthor] = useState('')
 
-        this.state = {
-            id: this.props.match.params.id,
-            name: '',
-            author: ''
-        }
-        this.changeNameHandler = this.changeNameHandler.bind(this)
-        this.changeAuthorHandler = this.changeAuthorHandler.bind(this)
-        this.updateBook = this.updateBook.bind(this)
-    }
-
-    componentDidMount() {
-        BookService.getBookById(this.state.id).then((res) => {
-            let book = res.data;
-            this.setState({ name: book.name, author: book.author})
+    useEffect(() => {
+        BookService.getBookById(id).then(res => {
+            const book = res.data
+            setName(book.name)
+            setAuthor(book.author)
         })
-    }
+    }, [id])
 
-    updateBook = (e) => {
+    const updateBook = (e) => {
         e.preventDefault()
 
-        let book = {name: this.state.name, author: this.state.author};
-        BookService.updateBook(book, this.state.id).then(res => {
-            this.props.history.push("/employees");
+        const book = {name, author};
+        BookService.updateBook(book, id).then(() => {
+            navigate("/books");
         })
     }
 
-    changeNameHandler = (e) => {
-        this.setState({ name: e.target.value })
+    const cancel = () => {
+        navigate("/books");
     }
 
-    changeAuthorHandler = (e) => {
-        this.setState({ author: e.target.value })
-    }
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="card col-md-6 offset-md-3 offset-md-3">
+                        <h3 className="text-center">Update Book</h3>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group">
+                                    <label> Name: </label>
+                                    <input placeholder="Name" name="name" className="form-control" value={this.state.name} onChange={this.changeNameHandler} />
+                                </div>
+                                <div className="form-group">
+                                    <label> Author: </label>
+                                    <input placeholder="Author" name="author" className="form-control" value={this.state.author} onChange={this.changeAuthorHandler} />
+                                </div>
 
-    cancel(){
-        this.props.history.push("/books");
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="card col-md-6 offset-md-3 offset-md-3">
-                            <h3 className="text-center">Update Book</h3>
-                            <div className="card-body">
-                                <form>
-                                    <div className="form-group">
-                                        <label> Name: </label>
-                                        <input placeholder="Name" name="name" className="form-control" value={this.state.name} onChange={this.changeNameHandler} />
-                                    </div>
-                                    <div className="form-group">
-                                        <label> Author: </label>
-                                        <input placeholder="Author" name="author" className="form-control" value={this.state.author} onChange={this.changeAuthorHandler} />
-                                    </div>
-
-                                    <button className="btn btn-success" onClick={this.updateBook}>Save</button>
-                                    <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
-                                </form>
-                            </div>
+                                <button className="btn btn-success" onClick={this.updateBook}>Save</button>
+                                <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default UpdateBookComponent
