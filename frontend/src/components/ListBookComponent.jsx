e16c29203d06a26cca2e2b5ab96a1feb0c6f02d9
jupitters <sampaiobale@gmail.com>
2026-02-12 18:14:35 -0300

    list books updated

--- frontend/src/components/ListBookComponent.jsx
+++ frontend/src/components/ListBookComponent.jsx
@@ -3,7 +3,7 @@ import BookService from "../services/BookService";
 import { useNavigate } from 'react-router-dom';
 
 const ListBookComponent = () => {
-    const[books, setBooks] = useState([]);
+    const [books, setBooks] = useState([]);
     const navigate = useNavigate();
 
     const deleteBook = (id) => {
@@ -30,42 +30,40 @@ const ListBookComponent = () => {
         navigate("/add-book/-1");
     }
 
-    render(){
-        return(
-            <div>
-                <h2 className="text-center">Books List</h2>
-                <div className="row">
-                    <button className="btn btn-primary" onClick={this.addBook}> Add Book</button>
-                </div>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Book Name</th>
-                                <th>Book Author</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.books.map(book =>
-                            <tr key={book.id}>
-                                <td>{book.name}</td>
-                                <td>{book.author}</td>
-                                <td>
-                                    <button onClick={() => this.editBook(book.id)} className="btn btn-info">Update</button>
-                                    <button style={{marginLeft: "10px"}} onClick={() => this.deleteBook(book.id)} className="btn btn-alert">Delete</button>
-                                    <button style={{marginLeft: "10px"}} onClick={() => this.viewBook(book.id)} className="btn btn-info">View</button>
-                                </td>
-                            </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-                </div>
+    return(
+        <div>
+            <h2 className="text-center">Books List</h2>
+            <div className="row">
+                <button className="btn btn-primary" onClick={addBook}> Add Book</button>
             </div>
-        )
-    }
+            <div className="row">
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Book Name</th>
+                            <th>Book Author</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        books.map((book) =>
+                        <tr key={book.id}>
+                            <td>{book.name}</td>
+                            <td>{book.author}</td>
+                            <td>
+                                <button onClick={() => editBook(book.id)} className="btn btn-info">Update</button>
+                                <button style={{marginLeft: "10px"}} onClick={() => deleteBook(book.id)} className="btn btn-alert">Delete</button>
+                                <button style={{marginLeft: "10px"}} onClick={() => viewBook(book.id)} className="btn btn-info">View</button>
+                            </td>
+                        </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default ListBookComponent
