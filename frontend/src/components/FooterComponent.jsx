ba1589f9553c6457a911cf358e731761728c02a5
jupitters <sampaiobale@gmail.com>
2026-02-12 18:04:27 -0300

    footer

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -1,23 +1,11 @@
-import React, { Component } from 'react'
-
-class FooterComponent extends Component{
-    constructor(props){
-        super(props)
-
-        this.state = {
-
-        }
-    }
-
-    render(){
-        return (
-            <div>
-                <footer className="footer">
-                    <span className="text-muted">All Rights Reserved </span>
-                </footer>
-            </div>
-        )
-    }
+function FooterComponent(){
+    return (
+        <div>
+            <footer className="footer">
+                <span className="text-muted">All Rights Reserved </span>
+            </footer>
+        </div>
+    )
 }
 
 export default FooterComponent
