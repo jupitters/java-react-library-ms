0a82d46a6a592e9944492b69b8a9f716a756cc92
jupitters <sampaiobale@gmail.com>
2026-02-12 19:00:15 -0300

    update book updated

--- frontend/src/components/UpdateBookComponent.jsx
+++ frontend/src/components/UpdateBookComponent.jsx
@@ -32,7 +32,7 @@ const UpdateBookComponent = () => {
         <div>
             <div className="container">
                 <div className="row">
-                    <div className="card col-md-6 offset-md-3>
+                    <div className="card col-md-6 offset-md-3">
                         <h3 className="text-center">Update Book</h3>
                         <div className="card-body">
                             <form>
