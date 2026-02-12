7b05bcc41af1d7f39920222878888bf40ad9da7b
jupitters <sampaiobale@gmail.com>
2026-02-12 18:02:13 -0300

    fix header

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class HeaderComponent{
-
-    render(){
-        return (
-            <div>
-                <header>
-                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                        <Link to="/" className="navbar-brand">Library Management App</Link>
-                    </nav>
-                </header>
-            </div>
-        )
-    }
+function HeaderComponent(){
+    return (
+        <div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <Link to="/" className="navbar-brand">Library Management App</Link>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default HeaderComponent
